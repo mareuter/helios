2cd0928815f8aa9d5f508d39f8b4e10566022fc0
Michael Reuter <mareuternh@gmail.com>
2023-03-06 22:17:39 -0700

    Change response model and add coordinate validation.

--- app/main.py
+++ app/main.py
@@ -4,11 +4,50 @@
 
 __all__ = ["app"]
 
-from fastapi import FastAPI
+import math
+from datetime import datetime
+from typing import Any
+
+from fastapi import FastAPI, Query
+from pydantic import BaseModel
+
+from .helios import Helios
 
 app = FastAPI()
+
+
+class SkyTransitions(BaseModel):
+    astronomical_dawn: float
+    nautical_dawn: float
+    civil_dawn: float
+    sunrise: float
+    sunset: float
+    civil_dusk: float
+    nautical_dusk: float
+    astronomical_dusk: float
 
 
 @app.get("/")
 async def root() -> dict:
     return {"msg": "This is a web service, nothing to see here."}
+
+
+@app.get("/sky_transitions")
+async def sky_transitions(
+    cdatetime: float,
+    tz: str,
+    lat: float = Query(
+        le=math.fabs(90.0),
+        title="latitude",
+        description="The location's latitude. North is positive. South is negative",
+    ),
+    lon: float = Query(
+        le=math.fabs(180.0),
+        title="latitude",
+        description="The location's latitude coordinate. East is positive. West is negative.",
+    ),
+) -> Any:
+    h = Helios()
+    st = h.sky_transitions(lat, lon, datetime.fromtimestamp(cdatetime), tz)
+    output = {k.replace(" ", "_").lower(): v.timestamp() for k, v in st.items()}
+    return output
--- tests/test_main.py
+++ tests/test_main.py
@@ -13,3 +13,36 @@ def test_root() -> None:
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"msg": "This is a web service, nothing to see here."}
+
+
+def test_sky_transitions() -> None:
+    response = client.get(
+        "/sky_transitions",
+        params={
+            "lat": 40.8939,
+            "lon": -83.8917,
+            "cdatetime": 1677880560.0,
+            "tz": "US/Eastern",
+        },
+    )
+    assert response.status_code == 200
+    assert response.json()["astronomical_dawn"] == 1677839749.146742
+
+
+def test_bad_location() -> None:
+    response = client.get(
+        "/sky_transitions",
+        params={
+            "lat": 96.5,
+            "lon": 184.342,
+            "cdatetime": 1677880560.0,
+            "tz": "US/Eastern",
+        },
+    )
+    assert response.status_code == 422
+    detail = response.json()["detail"]
+    for info in detail:
+        if "lat" in info["loc"]:
+            assert info["msg"] == "ensure this value is less than or equal to 90.0"
+        if "lon" in info["loc"]:
+            assert info["msg"] == "ensure this value is less than or equal to 180.0"
