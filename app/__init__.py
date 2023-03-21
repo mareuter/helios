6cee4b6d914425b3c6b1ff74d7999980fe05a7f5
Michael Reuter <mareuternh@gmail.com>
2023-03-20 17:37:39 -0700

    Update version.

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,6 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
-__version__ = "0.1.dev0"
+__version__ = "0.1.2"
 __author__ = "Michael Reuter"
 __email__ = "mareuternh@gmail.com"
